156dee7d9f884f7de58332faee0f8eb98d5b1ffb
adam <adam.yang121@gmail.com>
2017-08-11 10:11:42 -0400

    add switch

--- app/components/App.js
+++ app/components/App.js
@@ -3,10 +3,10 @@ var Popular = require('./Popular');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 var Nav = require('./Nav');
 var Home = require('./Home');
-
-
+var Battle = require('./Battle');
 
 class App extends React.Component{
 	render(){
@@ -14,8 +14,14 @@ class App extends React.Component{
 			<Router>
 				<div className="container">
 					<Nav />
+					<Switch>
 					<Route exact path='/' component={Home} />
+					<Route exact path='/battle' component={Battle} />
 					<Route path='/popular' component={Popular} />
+					<Route render={function(){
+						return <p>Oops! Wrong Place!</p>
+					}}/>
+					</Switch>
 				</div>
 			</Router>
 		)
--- app/components/Battle.js
+++ app/components/Battle.js
@@ -2,7 +2,13 @@ var React = require('react');
 
 class Battle extends React.Component {
 	render() {
-
-		
+		return(
+			<div>
+				Battle!!
+			</div>
+			)
 	}
 }
+
+
+module.exports = Battle;
